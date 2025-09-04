00f11268160cf9c44ef8ca189dde59daf8a6eaef
fndeveloper <fasihnasir60@gmail.com>
2025-09-04 19:54:41 +0500

    c

--- js/app.js
+++ js/app.js
@@ -145,7 +145,7 @@ setTimeout(() => {
   loader_div.style.display="none";
   document.body.classList.add("anim")
   document.body.style.overflow = "visible"; 
-},  4000);
+},  0);
  
     // ==== LOADER REMOVE CODE END ==========
 
